9865206638bc0d73b9e0ad7a010a133d411ac1fe
Uyen Truong <haiuyentruong@gmail.com>
2019-03-03 23:44:36 +0200

    Parse string, int, array, double, boolean

--- utjson.c
+++ utjson.c
@@ -28,7 +28,7 @@ JSON *parse_boolean(char *buf);
 JSON *parse_num_int(char *buf);
 JSON *parse_num_dbl(char *buf);
 JSON *parse_string(char *buf);
-JSON *parse_array(char *buf);
+JSON *parse_array(char **buf);
 int   prepend(JSON **head, JSON *item);
 void  reverse_list(JSON **head);
 int   list_length(JSON *head);
@@ -37,61 +37,9 @@ int   is_whitespace(char c);
 char *rm_leading_whitespaces(char *buf);
 int   is_structural(char c);
 JSON *which_type(char *token);
-
-int token_type(char *token)
-{
-    if (token[0] == '"')
-        return JSON_STRING;
-    else if (strcmp(token, "true") == 0 || strcmp(token, "false") == 0)
-        return JSON_BOOLEAN;
-    else if (isdigit(token[0]))
-        return JSON_NUM_INT;
-    return -1;
-}
-
-void print_json(JSON *json)
-{
-    // assume only int
-    printf("type %d, data %d\n", json->type, *(int *)json->data);
-}
-
-JSON *parse_array_of_int(char *buf)
-{
-    char  token[8192] = { '\0' };
-    char *p = buf;
-    JSON *head = NULL;
-
-    JSON *array = malloc(sizeof(JSON));
-
-    array->type = JSON_ARRAY;
-    array->next = NULL;
-
-    while ((p = tokenizer(p, token)) != NULL)
-    {
-        int t = token_type(token);
-        if (t == JSON_NUM_INT)
-        {
-            JSON *e = parse_num_int(token);
-            prepend(&head, e);
-        }
-        else if (token[0] == ',')
-            ;
-        else if (token[0] == ']')
-        {
-            reverse_list(&head);
-            array->size = list_length(head);
-            array->data = head;
-        }
-    }
-
-    // print out the list
-    for (JSON *j = head; j != NULL; j = j->next)
-    {
-        print_json(j);
-    }
-    return array;
-}
-
+char *show_type(JSON *item);
+int   token_type(char *token);
+void print_json(JSON *json);
 
 int main(int argc, char **argv)
 {
@@ -122,14 +70,10 @@ int main(int argc, char **argv)
     for (i = 0; i < size; i++)
         printf("%c", buf[i]);
 
-    // Parse num int
-    // JSON *num_int = parse_num_int(buf);
-    // printf("### Type %d Size %d Data %d\n", num_int->type, num_int->size, *(int *)num_int->data);
-
     // Parse tokens
     char  token[8192] = { '\0' };
     char *p = buf;
-    // JSON *part;
+    JSON *a;
 
     while ((p = tokenizer(p, token)) != NULL)
     {
@@ -137,12 +81,9 @@ int main(int argc, char **argv)
         if (token[0] == '[')
         {
             printf("shit\n");
-            JSON *a = parse_array_of_int(p);
+            a = parse_array(&p);
+            print_json(a);
         }
-        // if ((part = which_type(token)) != NULL)
-        // {
-        //     printf("### Type %d Size %d\n", part->type, part->size);
-        // }
     }
 
     // Push and pop structural tokens
@@ -168,11 +109,6 @@ int main(int argc, char **argv)
     fclose(fp);
     return 0;
 }
-
-
-
-
-
 
 JSON *parse_boolean(char *token)
 {
@@ -222,20 +158,63 @@ JSON *parse_string(char *str)
 
     string->type = JSON_STRING;
     string->size = strlen(str);
-    string->data = str;
+    string->data = strdup(str);
     string->next = NULL;
 
     return string;
 }
 
-JSON *parse_array(char *buf)
+JSON *parse_array(char **buf)
 {
+    char  token[8192] = { '\0' };
+    char *p = *buf;
+    JSON *head = NULL;
+    JSON *e;
+
     JSON *array = malloc(sizeof(JSON));
 
     array->type = JSON_ARRAY;
     array->next = NULL;
-    // array->size =
 
+    while ((p = tokenizer(p, token)) != NULL)
+    {
+        int t = token_type(token);
+        if (t == JSON_NUM_INT)
+        {
+            e = parse_num_int(token);
+            prepend(&head, e);
+        }
+        else if (t == JSON_NUM_DBL)
+        {
+            e = parse_num_dbl(token);
+            prepend(&head, e);
+        }
+        else if (t == JSON_BOOLEAN)
+        {
+            e = parse_boolean(token);
+            prepend(&head, e);
+        }
+        else if (t == JSON_STRING)
+        {
+            e = parse_string(token);
+            prepend(&head, e);
+        }
+        else if (token[0] == ',')
+            ;
+        else if (token[0] == '[')
+        {
+            e = parse_array(&p);
+            prepend(&head, e);
+        }
+        else if (token[0] == ']')
+        {
+            reverse_list(&head);
+            array->size = list_length(head);
+            array->data = head;
+            *buf = p;
+            break;
+        }
+    }
     return array;
 }
 
@@ -392,4 +371,82 @@ JSON *which_type(char *token)
     }
 
     return p;
+}
+
+char *show_type(JSON *item)
+{
+    switch (item->type)
+    {
+        case JSON_OBJECT:
+            return "JSON_OBJECT";
+        case JSON_ARRAY:
+            return "JSON_ARRAY";
+        case JSON_BOOLEAN:
+            return "JSON_BOOLEAN";
+        case JSON_NUM_INT:
+            return "JSON_NUM_INT";
+        case JSON_NUM_DBL:
+            return "JSON_NUM_DBL";
+        case JSON_STRING:
+            return "JSON_STRING";
+        default:
+            return "Unknown";
+    }
+}
+
+int token_type(char *token)
+{
+    int   is_int = 1;
+    int   len    = strlen(token);
+
+    if (token[0] == '"')
+        return JSON_STRING;
+    else if (strcmp(token, "true") == 0 || strcmp(token, "false") == 0)
+        return JSON_BOOLEAN;
+    else if (isdigit(token[0]))
+    {
+        for (int i = 0; i < len; i++)
+        {
+            if (token[i] == '.' || token[i] == 'e' || token[i] == 'E')
+                is_int = 0;
+        }
+
+        if (is_int)
+            return JSON_NUM_INT;
+        else
+            return JSON_NUM_DBL;
+    }
+    else if (strcmp(token, "[") == 0)
+        return JSON_ARRAY;
+
+    return -1;
+}
+
+void print_json(JSON *json)
+{
+    // assume only int
+    switch(json->type)
+    {
+        case JSON_ARRAY:
+            printf("type %s\n", show_type(json));
+            for (JSON *p = json->data; p != NULL; p = p->next)
+                print_json(p);
+            break;
+        case JSON_BOOLEAN:
+            printf("type %s, data %s\n", show_type(json),
+                   *(int *)json->data ? "true" : "false");
+            break;
+        case JSON_NUM_INT:
+            printf("type %s, data %d\n", show_type(json), *(int *)json->data);
+            break;
+        case JSON_NUM_DBL:
+            printf("type %s, data %f\n", show_type(json), *(double *)json->data);
+            break;
+        case JSON_STRING:
+            printf("type %s, data %s\n", show_type(json), json->data);
+            break;
+        default:
+            printf("Unknown\n");
+            break;
+    }
 }
