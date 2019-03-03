f9c9f6cdc576104e65033383ef2a697c5997a273
Uyen Truong <haiuyentruong@gmail.com>
2019-03-03 17:00:13 +0200

    Parse string, boolean, int, double from each token

--- utjson.c
+++ utjson.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 typedef enum json_type {
     JSON_OBJECT  = 0,
@@ -15,6 +16,7 @@ typedef struct json {
     int   type;
     int   size;
     void *data;
+    struct json *next;
 } JSON;
 
 typedef struct kv {
@@ -26,6 +28,15 @@ JSON *parse_boolean(char *buf);
 JSON *parse_num_int(char *buf);
 JSON *parse_num_dbl(char *buf);
 JSON *parse_string(char *buf);
+// JSON *parse_array(char *buf);
+int   prepend(JSON **head, JSON *item);
+void  reserve(JSON **head);
+int   list_length(JSON *head);
+char *tokenizer(char *buf_in, char *token);
+int   is_whitespace(char c);
+char *rm_leading_whitespaces(char *buf);
+int   is_structural(char c);
+JSON *which_type(char *token);
 
 int main(int argc, char **argv)
 {
@@ -38,7 +49,6 @@ int main(int argc, char **argv)
         printf("File not found\n");
         return -1;
     }
-    printf("Read file\n");
 
     fseek(fp, 0L, SEEK_END);
     int size = ftell(fp);
@@ -60,33 +70,18 @@ int main(int argc, char **argv)
     // JSON *num_int = parse_num_int(buf);
     // printf("### Type %d Size %d Data %d\n", num_int->type, num_int->size, *(int *)num_int->data);
 
-    // Parse num double
-    // JSON *num_dbl = parse_num_dbl(buf);
-    // printf("### Type %d Size %d Data %.3f\n", num_dbl->type, num_dbl->size, *(double *)num_dbl->data);
+    // Parse tokens
+    char  token[8192] = { '\0' };
+    char *p = buf;
+    JSON *part;
 
-    // Parse string
-    // char buf_str[8192];
-    // for (i = 1, j = 0; i < size; i++, j++)
-    // {
-    //     if (buf[i] != '"')
-    //         buf_str[j] = buf[i];
-    //     else
-    //         buf_str[j] = '\0';
-    // }
-    // JSON *str = parse_string(buf_str);
-    // printf("### Type %d Size %d Data %s\n", str->type, str->size, str->data);
-
-    // Parse boolean
-    char buf_str[8192];
-    for (i = 0; i < size; i++)
+    while ((p = tokenizer(p, token)) != NULL)
     {
-        if (buf[i] != '\n')
-            buf_str[i] = buf[i];
-        else
-            buf_str[i] = '\0';
+        printf("%s\n", token);
+        part = which_type(token);
+        printf("### Type %d Size %d\n", part->type, part->size);
     }
-    JSON *boolean = parse_boolean(buf_str);
-    printf("### Type %d Size %d Data %i\n", boolean->type, boolean->size, *(char *)boolean->data);
+
     // Push and pop structural tokens
     char stack[128] = { '\0' };
     for (i = 0; i < size; i++)
@@ -107,8 +102,6 @@ int main(int argc, char **argv)
         }
     }
 
-    free(boolean->data);
-    free(boolean);
     fclose(fp);
     return 0;
 }
@@ -124,6 +117,7 @@ JSON *parse_boolean(char *buf)
         *(char *)boolean->data = 1;
     else if (strcmp(buf, "false") == 0)
         *(char *)boolean->data = 0;
+    boolean->next = NULL;
 
     return boolean;
 }
@@ -136,6 +130,7 @@ JSON *parse_num_int(char *buf)
     num_int->size = sizeof(int);
     num_int->data = malloc(sizeof(int));
     *(int *)num_int->data = atoi(buf);
+    num_int->next = NULL;
 
     return num_int;
 }
@@ -143,12 +138,12 @@ JSON *parse_num_int(char *buf)
 JSON *parse_num_dbl(char *buf)
 {
     JSON *num_dbl = malloc(sizeof(JSON));
-    char *ptr;
 
     num_dbl->type = JSON_NUM_DBL;
     num_dbl->size = sizeof(double);
     num_dbl->data = malloc(sizeof(double));
-    *(double *)num_dbl->data = strtod(buf, &ptr);
+    *(double *)num_dbl->data = atof(buf);
+    num_dbl->next = NULL;
 
     return num_dbl;
 }
@@ -160,6 +155,167 @@ JSON *parse_string(char *str)
     string->type = JSON_STRING;
     string->size = strlen(str);
     string->data = str;
+    string->next = NULL;
 
     return string;
+}
+
+// JSON **parse_array(char *buf)
+// {
+//     JSON **aray = malloc(sizeof(JSON *));
+//
+//     array->type = JSON_ARRAY;
+//     array->size =
+//
+//     return array;
+// }
+
+int prepend(JSON **head, JSON *item)
+{
+    if (item == NULL)
+        return -1;
+
+    item->next = *head;
+    *head = item;
+
+    return 0;
+}
+
+void reserve(JSON **head)
+{
+    JSON *prev = NULL;
+    JSON *current = *head;
+    JSON *next;
+
+    while (current != NULL)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    *head = prev;
+}
+
+int list_length(JSON *head)
+{
+    int len = 0;
+
+    for (JSON *p = head; p != NULL; p = p->next)
+        len++;
+
+    return len;
+}
+
+char *tokenizer(char *buf_in, char *token)
+{
+    char *buf       = rm_leading_whitespaces(buf_in);
+    int   j         = 0;
+    int   is_string = 0;
+    int   size      = strlen(buf);
+    int   i;
+
+    for (i = 0; i < size; i++)
+    {
+        if (!is_string)
+        {
+            if (is_structural(buf[i])) // true
+            {
+                token[0] = buf[i];
+                break;
+            }
+            else
+            {
+                if (buf[i] == '"')
+                    is_string = 1;
+
+                if (buf[i] != ' ')
+                {
+                    token[j] = buf[i];
+                    j++;
+                }
+                if (is_structural(buf[i + 1]))
+                {
+                    j--;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            token[j] = buf[i];
+            j++;
+
+            if (buf[i] == '"')
+            {
+                is_string = 0;
+                j--;
+                break;
+            }
+        }
+    }
+
+    if (strlen(buf) == 0)
+        return NULL;
+
+    token[j + 1] = '\0';
+    return buf + i + 1;
+}
+
+int is_whitespace(char c)
+{
+    if (c == ' ' || c == '\t' || c == '\r' || c == '\n')
+        return 1;
+    return 0;
+}
+
+char *rm_leading_whitespaces(char *buf)
+{
+    char *p;
+    for (p = buf; *p; p++)
+    {
+        if (!is_whitespace(*p))
+            break;
+    }
+    return p;
+}
+
+int is_structural(char c)
+{
+    if (c == '[' || c == ',' || c == '{' || c == ']' || c == '}' || c == ':')
+        return 1;
+    return 0;
+}
+
+JSON *which_type(char *token)
+{
+    JSON *p;
+    int   is_int = 1;
+    int   len    = strlen(token);
+
+    if (token[0] == '"')        // String
+        p = parse_string(token);
+    else if (strcmp(token, "true") || strcmp(token, "false"))
+        p = parse_boolean(token);
+    else if (isdigit(token[0]))
+    {
+        for (int i = 0; i < len; i++)
+        {
+            if (token[i] == '.')
+                is_int = 0;
+        }
+
+        if (is_int)
+            p = parse_num_int(token);
+        else
+            p = parse_num_dbl(token);
+    }
+    else
+    {
+        printf("Unknown tokens\n");
+        p = NULL;
+    }
+
+    return p;
 }
